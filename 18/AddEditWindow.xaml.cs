f64526871787931b8a53415efb3cf42aed16ece5
Валерия <leralera050505@mail.ru>
2023-11-23 08:58:47 +0300

    3

---
+++ 18/AddEditWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _18
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditWindow.xaml
+    /// </summary>
+    public partial class AddEditWindow : Window
+    {
+        public AddEditWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Service service = new Service(Convert.ToInt32(tbId.Text), tbName.Text, Convert.ToDateTime(dpDate.Text));
+            ServiceList.services.Add(service);
+            this.Close();
+        }
+    }
+}
