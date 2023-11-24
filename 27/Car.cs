234915e21db829567e1dad0339a9ad681b715018
Валерия <leralera050505@mail.ru>
2023-11-24 15:10:07 +0300

    27

---
+++ 27/Car.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _27
+{
+    class Car
+    {
+        public static List<Car> cars = new List<Car>()
+        {
+            new Car( 2 ,"волга",  new DateTime(2023, 10, 06), "Алексей Алексей", -25)
+        };
+
+        public int Number { get; set; }
+        public string NameCar { get; set; }
+        public DateTime DateService { get; set; }
+        public string FIO { get; set; }
+        public int Age { get; set; }
+
+
+        public Car(int number, string namecar, DateTime dateservice, string fio, int age)
+        {
+            Number = number;
+            NameCar = namecar;
+            DateService = dateservice;
+            FIO = fio;
+            Age = age;
+        }
+
+        public static void CheckNumber(int number)
+        {
+            if (number < 0)
+            {
+                MessageBox.Show("Номер не может быть пустым");
+            }
+        }
+        public static void CheckNameCar(string namecar)
+        {
+            if (string.IsNullOrEmpty(namecar))
+            {
+                MessageBox.Show("Название машины не может быть пустым");
+            }
+        }
+        public static void CheckDateService(DateTime dateservice)
+        {
+            if (dateservice < DateTime.Now)
+            {
+                MessageBox.Show("Нельзя записаться на дату,которая уже была");
+            }
+        }
+        public static void CheckFIO(string fio)
+        {
+            if (string.IsNullOrEmpty(fio))
+            {
+                MessageBox.Show("Нельзя записаться на дату,которая уже была");
+            }
+        }
+        public static void CheckAge(int age)
+        {
+            if (age < 18)
+            {
+                MessageBox.Show("Нельзя записаться несовершеннолетнему");
+            }
+        }
+    }
+}
---
+++ 27/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _27
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            lwCar.ItemsSource = Car.cars;
+            foreach (var item in Car.cars)
+            {
+                Car.CheckAge(item.Age);
+                Car.CheckNumber(item.Number);
+                Car.CheckNameCar(item.NameCar);
+                Car.CheckDateService(item.DateService);
+                Car.CheckFIO(item.FIO);
+            }
+        }
+    }
+}
